62f5de3ae361f91f508807c770264354eff39273
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-22 12:50:52 +0000

    w3.1

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * print_sign - Entry point.
+ * @n:the int to check
+ * Return: Always 0 (Success)
+ */
+print_sign(int n)
+{
+
+	if (n >= 0)
+		_putchar('+');
+		return (1);
+	else if (n <= 0)
+		 _putchar('-');
+		return (-1);
+	else if (n <= 0)
+		_putchar('0');
+	        return (0);
+
+	_putchar('\n');
+}
