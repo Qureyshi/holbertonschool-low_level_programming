a1b913784188ab39b441f50562e52efa2f340f3f
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-22 14:04:50 +0000

    w3.1

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * print_alphabet - Entry point.
+ * @n: number
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	int a, b;
+	
+	if (n <= 98)
+		for (a = n ; a <= 98 ; a++)
+			_putchar(a + '0');
+			_putchar(', ');
+	if (n >= 98)
+                for (b = n ; b >= 98 ; b--)
+	                _putchar(b + '0');
+			_putchar(', ');
+
+	_putchar('\n');
+}
