cfbc7978153365b951a130462bd2a703337b275b
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-22 13:21:52 +0000

    w3.1

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * _abs - Entry point.
+ * @n: abs
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+
+}
