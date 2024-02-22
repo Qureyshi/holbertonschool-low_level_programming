1f5608521daa9e88864bb8285513ee4f8ee29d67
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-22 12:18:00 +0000

    w3.1

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 	char letter;
 	int i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{	
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
