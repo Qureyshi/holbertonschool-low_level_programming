f1efd36a5bf87d007198dd2739a026e46cd21f16
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-22 13:49:44 +0000

    w3.1

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * add - Entry point.
+ * @n: sum
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
