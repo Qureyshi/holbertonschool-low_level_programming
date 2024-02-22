b5e18f9a165ae426cac571ebf1aa741b8484d4c0
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-22 12:12:15 +0000

    w3.1

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{	
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+	}
+	_putchar('\n');
+}
