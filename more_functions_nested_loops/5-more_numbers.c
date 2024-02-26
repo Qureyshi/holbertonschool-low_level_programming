6e22ee67fb4f7a50d1b407e7b6ae3dc9977037d0
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-26 13:43:18 +0000

    task5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,18 @@
  */
 void more_numbers(void)
 {
-	int c;
+	int c, j;
 
-	for (c = 0; c <= 14; c++)
+	for (j = 0; j < 10 ; j++)
 	{
-		if (c > 9)
-			_putchar('1');
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+				_putchar('1');
 
-		_putchar(c % 10 + '0');
-	}
+			_putchar(c % 10 + '0');
+		}
 
 	_putchar('\n');
+	}
 }
