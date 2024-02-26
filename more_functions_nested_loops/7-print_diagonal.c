d480920234ccef93d8bf36f17e79c6ccfa6ab4b4
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-26 14:16:13 +0000

    task7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - uppercase letters
+ * @n: param
+ *
+ * Return: 0 or 1
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n ; i++)
+	{
+		for (j = 0; j < i ; j++)
+			 _putchar(' ');
+
+		_putchar('\\');
+		_putchar('\n');
+	}
+
+}
