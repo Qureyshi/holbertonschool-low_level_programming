06a45c85cb4cf2e05b664ba53e7d562257fcbb57
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-26 14:55:40 +0000

    task10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - uppercase letters
+ * @size: param
+ *
+ * Return: 0 or 1
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < size ; i++)
+	{
+		for (j = 0; j < size - i ; j++)
+		{
+			_putchar(' ');
+		}
+
+		for (k = 0; k < i ; k++)
+                {
+                        _putchar('#');
+                }
+
+		_putchar('\n');
+	}
+
+}
