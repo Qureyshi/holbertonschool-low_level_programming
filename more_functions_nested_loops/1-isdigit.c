ee2c363cf4de6e2e4929bf970165a4ff93c2aecd
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-26 12:33:28 +0000

    task1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _isupper - uppercase letters
+ * _isdigit - uppercase letters
  * @c: character to check
  *
  * Return: 0 or 1
  */
-int _ist(int c)
+int _isdigit(int c)
 {
 	if (c >= 0 && c <= 9)
 		return (1);
