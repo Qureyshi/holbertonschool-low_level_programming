7f0b961f17ec3e92a09d905f228110c04ca78903
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-26 13:39:51 +0000

    task5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - uppercase letters
+ *
+ * Return: 0 or 1
+ */
+void more_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 14; c++)
+	{
+		if (c > 9)
+			_putchar('1');
+
+		_putchar(c % 10 + '0');
+	}
+
+	_putchar('\n');
+}
