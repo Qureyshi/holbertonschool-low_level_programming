5bbef6a8fa1bf9a7f5579f966e1ce0af8846bdd5
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-27 13:22:33 +0000

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * -puts_half charreturns the length of a string
+ * @str: string to evaluate
+ *
+ * Return: the length of the string
+ */
+void puts_half(char *str)
+{
+	int i, j, len;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+			_putchar(str[i]);
+	}
+	else if (len % 2 != 0)
+	{
+		for (i = (len - 1) / 2; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
