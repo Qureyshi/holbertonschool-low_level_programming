4e507a784fcbc6d4dd8fa590e0e19e54bd5e208c
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-27 11:32:22 +0000

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - returns the length of a string
+ * @str: string to evaluate
+ *
+ * Return: the length of the string
+ */
+void _puts(char *str);
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+
+}
