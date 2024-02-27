d03658729d727653607f77e28f5dda7a85f431f7
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-27 11:01:25 +0000

    1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - updates the value of the variable
+ * the pointer points to to 98
+ * @a: pointer to the variable to update
+ * @b: pointer to the variable to update
+ */
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	*c = *a;
+	*a = *b;
+	*b = *c;
+}
