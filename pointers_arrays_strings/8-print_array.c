06c9bc8501c0aa73411d88aa2dd1e98733d21d67
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-27 13:58:40 +0000

    8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - charreturns the length of a string
+ * @a: string to evaluate
+ * @n: string to evaluate
+ *
+ * Reiturn: the length of the string
+ */
+void print_array(int *a, int n)
+{
+	int j;
+
+	for (j = 0 ; j < n ; j++)
+	{
+		if (j == 0)
+			printf("%d", a[j]);
+		else
+			printf(", %d", a[j]);
+	}
+
+	printf("\n");
+}
+
