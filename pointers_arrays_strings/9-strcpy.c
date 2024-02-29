f0129f5b92363f633ec1d74429bcf76fe19ca184
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-29 17:09:46 +0000

    task0

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - concatenate two strings
+ * @dest: char string to concatenate to
+ * @src: char string
+ * Return: pointer to resulting string `dest`
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[c];
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
