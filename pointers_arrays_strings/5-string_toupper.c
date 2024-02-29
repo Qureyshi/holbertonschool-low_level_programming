fa016d8be6320cdd5c5dd3a8858dd49a80b1a459
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-29 14:06:57 +0000

    task0

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *string_toupper - concatenate two strings
+ * @a: char string to concatenate to
+ * @n: char string
+ *
+ * Return: pointer to resulting string `dest`
+ */
+
+char *string_toupper(char *)
+{
+	int i;
+	for (i = 0; *[i] != '\0'; i++)
+	{
+		if (*[i] > 96 && c[i] < 123)
+		{
+			*[i] -= 32;
+		}
+	}
+
+	return (*);
+
+}
