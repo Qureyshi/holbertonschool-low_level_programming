f38e4a5c9d8d4eea7b21a2802e1efaae4c78f0a6
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-29 14:21:21 +0000

    task0

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * cap_string - concatenate two strings
+ * @c: char string to concatenate to
+ *
+ * Return: pointer to resulting string `dest`
+ */
+
+char *cap_string(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] > 96 && c[i] < 123)
+		{
+			c[i] -= 32;
+		}
+	}
+
+	return (c);
+
+}
