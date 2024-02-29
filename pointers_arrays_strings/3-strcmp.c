e35b3214b5ba7dce1304d02a4f816e78add52942
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-29 13:28:05 +0000

    task0

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - concatenate two strings
+ * @s1: char string to concatenate to
+ * @s2: char string
+ * 
+ * Return: pointer to resulting string `dest`
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int c;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+
+	return (0);
+}
