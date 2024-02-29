8aa4f96083499863b263f4b2f7332b43a0660566
Qureyshi <kamranqureyshi1998@gmail.com>
2024-02-29 12:50:27 +0000

    task0

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - concatenate two strings
+ * @dest: char string to concatenate to
+ * @src: char string
+ * @n: n param
+ * Return: pointer to resulting string `dest`
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (c = 0; c < n && src[c] != '\0'; c++)
+	{
+		dest[c] = src[c];
+	}
+
+	dest[n] = '\0';
+	return (dest);
+}
