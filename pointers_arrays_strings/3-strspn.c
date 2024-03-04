2904596361067449f78bc640d1b4fb83e02397af
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-04 16:27:36 +0000

    t2

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -12,8 +12,8 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i, j, k;
 	k = 0;
 
-	for (i = 0; s[i] != '\0' ; i++)
-		for (j = 0; accept[j] = '\0' ; j++)
+	for (i = 0; s[i] != '\0' && s[i] != ' '; i++)
+		for (j = 0; accept[j] != '\0' ; j++)
 			if (s[i] == accept[j])
 				k++;
 
