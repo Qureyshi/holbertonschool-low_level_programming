01fab148bbb7cf32740c7ab06565e01891c60530
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-04 17:11:47 +0000

    t2

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -10,12 +10,9 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
-	char* k;
 
-	for (i = 0; s[i] != '\0' && s[i] != ' '; i++)
+	for (i = 0; s[i] != '\0'; i++)
 		for (j = 0; accept[j] != '\0' ; j++)
 			if (s[i] == accept[j])
-				k = &s[i]-2;
-
-	return (k);
+				return (s + i);
 }
