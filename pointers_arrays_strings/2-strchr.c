a139608865c657c3ddd970d1a5bb63f5404c7c24
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-04 15:45:51 +0000

    t2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr- returns the length of a string
+ * @s: string to evaluate
+ * @c: string to evaluate
+ *
+ * Return: the length of the string
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0' && s[i] != c; i++)
+		;
+	
+	if (s[i] == c)
+		return (s + i);
+
+	return (NULL);
+}
