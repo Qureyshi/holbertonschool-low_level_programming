3b5e871d48fe05d3003d8573d3d97385fd0df288
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-04 16:16:40 +0000

    t2

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strspn- returns the length of a string
+ * @s: string to evaluate
+ * @c: string to evaluate
+ *
+ * Return: the length of the string
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, k;
+	k = 0;
+
+	for (i = 0; s[i] != '\0' ; i++)
+		for (j = 0; accept[j] = '\0' ; j++)
+			if (s[i] == accept[j])
+				k++;
+
+	return (k);
+}
