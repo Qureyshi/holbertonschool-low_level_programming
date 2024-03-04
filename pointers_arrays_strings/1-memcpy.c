73396d3c06ebfd2faafbce9f3164a12994880e5b
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-04 14:53:40 +0000

    t1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,12 @@
  *
  * Return: the length of the string
  */
-char *_memcpy(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		s[i] = b[i];
+		dest[i] = src[i];
 
-	return (s);
+	return (dest);
 }
