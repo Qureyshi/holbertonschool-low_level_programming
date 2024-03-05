404e7db8f24b70f18c54addf6cb8c399c31c7b23
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-05 14:18:19 +0000

    r0

---
+++ recursion/aa.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns the length of a string
+ * @x: string to evaluate
+ * @y: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int check(int m, int i)
+{	if (n % i == 0)
+		return (0);
+	if (i >= m/2)
+		return (1);
+	else
+		return check(m, i + 1);
+
+
+}
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	else
+		return check(n, 2);
+}
+
