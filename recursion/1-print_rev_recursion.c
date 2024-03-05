f66f4e6fc30624def928faa587d573aeac18291f
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-05 12:13:56 +0000

    r0

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		_puts_recursion(s + 1);
+		_putchar(s[0]);
+	}
+}
+
