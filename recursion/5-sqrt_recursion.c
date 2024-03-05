e183c69ca100e5d2276fd4aebd15a2699e1ca76e
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-05 13:11:27 +0000

    r0

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  _sqrt_recursion - returns the length of a string
+ * @n: string to evaluate
+ *
+ * Return: the length of the string
+ */
+
+int _sqrt_recursion(int n)
+{
+	int a;
+	a = 0;
+
+	if ( a*a == n)
+		return (a);
+	else
+		a++;
+		_sqrt_recursion(n);
+
+}
+
