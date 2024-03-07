6c00eaeb0a0003cd57f03ec8bf8b125b67bd0f9f
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-07 12:33:49 +0000

    argc_argv

---
+++ argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - returns the length of a string
+ * @argc: string to evaluate
+ * @argv: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int main(int argc, char *argv[])
+{	
+	int sum;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] ; j++)
+			{
+				if (argv[num][digit] < '0' || argv[num][digit] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+}
