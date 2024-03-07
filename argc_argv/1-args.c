bf013e870aad7e628478e449a85029bef1fba9e6
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-07 11:51:02 +0000

    argc_argv

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - returns the length of a string
+ * @argc: string to evaluate
+ * @argv: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", arg);
+
+	return (0);
+}
