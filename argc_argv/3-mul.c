2bec417098c5e56ffa91e80cd9301eaefcddd3d5
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-07 12:15:41 +0000

    argc_argv

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -12,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	if( argc == 3)
 	{
-		printf("%d\n", (unsigned int)(argv[1]) * (unsigned int)(argv[2]));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
 	else	
