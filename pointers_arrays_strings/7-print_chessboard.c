b4e0c87e81bdbf94caf16b0808258d903f3eeb7c
Qureyshi <kamranqureyshi1998@gmail.om>
2024-03-08 11:46:52 +0000

    pointer

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -3,16 +3,16 @@
 #include <stdio.h>
 /**
  * print_chessboard- returns the length of a string
- * @a: string 
+ * @a: string
  *
  * Return: the length of the string
  */
 void print_chessboard(char (*a)[8])
 {	int i, j;
 
-	for(i = 0; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for(j = 0; j < 8; j++)
+		for (j = 0; j < 8; j++)
 		{
 			if (j == 7)
 			{
