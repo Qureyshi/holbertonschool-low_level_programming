14d37c413edc32176510d85de48694291facc3b8
Qureyshi <kamranqureyshi1998@gmail.om>
2024-03-08 12:28:15 +0000

    pointer

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,7 +9,7 @@
  * Return: the length of the string
  */
 void print_diagsums(int *a, int size)
-{	
+{
 	int i = 0;
 	int j = size - 1;
 	int sum1 = 0;
@@ -20,12 +20,12 @@ void print_diagsums(int *a, int size)
 		sum1 += a[i];
 		i = i + size + 1;
 
-	}	
-	
+	}
+
 	while (j < (size * size - 1))
 	{
 		sum2 += a[j];
-		j = j + size -1;
+		j = j + size - 1;
 	}
 
 	printf("%d, %d\n", sum1, sum2);
