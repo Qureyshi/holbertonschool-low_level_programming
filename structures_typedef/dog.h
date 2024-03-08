882a053348025a84e5ea337ebd6037e187902a0a
Qureyshi <kamranqureyshi1998@gmail.om>
2024-03-08 12:35:56 +0000

    struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,11 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 /**
- *  * struct dog - describes the sweet boy or girl
- *   * @name: name of dog
- *    * @age: how old is that dog?
- *     * @owner: who is the lucky person that gets to live with that dog?
- *      */
+ * struct dog - describes the sweet boy or girl
+ * @name: name of dog
+ * @age: how old is that dog?
+ * @owner: who is the lucky person that gets to live with that dog?
+ */
 struct dog
 {
 	char *name;
