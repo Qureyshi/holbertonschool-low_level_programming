00526551df2f86b086898d4dbfc7883cedc57fc9
Qureyshi <kamranqureyshi1998@gmail.om>
2024-03-08 11:26:16 +0000

    pointer

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strstr- returns the length of a string
+ * @haystack: string to evaluate
+ * @needle: string to evaluate
+ *
+ * Return: the length of the string
+ */
+char *_strstr(char *haystack, char *needle)
+{	
+	char *ret;
+
+	ret = strstr(haystack, needle);
+
+	return (0);
+
+}
