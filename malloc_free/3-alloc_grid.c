fe3c48aeab4eab10842de99a4d2ef47b652747dc
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-12 13:18:13 +0000

    malloc

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - create an array of chars, and initialize with specific char
+ * @width: size of array
+ * @height: parameter
+ * Return: char pointer to malloc created memory address or NULL if error
+ */
+int **alloc_grid(int width, int height)
+{
+	int **a;
+	int i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	a = malloc((width * height) * sizeof(int));
+
+	if (a == NULL)
+		return (NULL);
+
+	for (int i = 0; i < r * c; i++)
+		ptr[i] = 0;
+
+	return (a);
+}
