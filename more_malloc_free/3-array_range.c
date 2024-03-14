f08f6a67f9d836803ebf5a21a7af52cc1b2f027e
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-14 16:27:21 +0000

    more_malloc

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -16,12 +16,15 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc((max - min + 1) * sizeof(int));
+	ptr = malloc(sizeof(int) * (max - min + 1));
 
 	if (ptr == NULL)
 		return (NULL);
+
 	for (i = 0; i < (max - min + 1); i++)
+	{
 		ptr[i] = min++;
+	}
 
 	return (ptr);
 }
