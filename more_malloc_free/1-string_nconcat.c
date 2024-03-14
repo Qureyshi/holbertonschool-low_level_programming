2ac2d99752dab1beffce37df112ea890be247e83
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-14 15:31:57 +0000

    more_malloc

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -21,11 +21,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for(len1 = 0; s1[len1] != '\0'; len1++)
+	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
-	for(len2 = 0; s2[len2] != '\0'; len2++)
+	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
-	
+
 	if (sign >= len2)
 	{
 		sign = len2;
@@ -42,8 +42,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		ptr[i] = s1[i];
 
 	for (j = 0; j < sign; j++)
-                ptr[i++] = s2[j];
-	
+		ptr[i++] = s2[j];
+
 	ptr[i] = '\0';
 
 	return (ptr);
