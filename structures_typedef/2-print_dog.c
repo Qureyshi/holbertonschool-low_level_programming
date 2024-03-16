587037151d2f0b44e664423c6ec8632d0aab4994
Qureyshi <kamranqureyshi@gmail.com>
2024-03-16 14:02:03 +0000

    struct

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+
+/**
+ * print_dog - initializes a structure of type dog
+ * @d: pointer to structure
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		if (d->age != NULL)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %.6f\n", d->age);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+}
