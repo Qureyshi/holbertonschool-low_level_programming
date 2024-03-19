599fa8342a5def199dda63d825ffece737f92092
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-19 12:50:52 +0000

    funcpointer

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -4,14 +4,14 @@
  * int_index - Print a name from a function pointer
  * @array: char string
  * @size: function pointer that takes a string argument
- * @action: function pointer that takes a string argument
+ * @cmp: function pointer that takes a string argument
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || array == NULL || cmp == NULL )
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
