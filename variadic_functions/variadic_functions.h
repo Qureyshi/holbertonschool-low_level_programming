b9ebe02b88cc28906598c0a187b136ff509a460b
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-22 12:57:47 +0000

    variadicfunc

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,12 +8,4 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 int _putchar(char c);
-
-/**
- * struct funckey - a key for picking functions to use for printing
- * in 3-print_all.c
- *
- * @f: function to use for printing
- * @spec: character specifier
- */
 #endif
