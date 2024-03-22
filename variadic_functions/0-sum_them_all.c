3ffafd14ecd780bde0e5c590397dabcd1926d330
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-22 12:53:02 +0000

    variadicfunc

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,14 +11,15 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int Sum = 0;
 	va_list ptr;
+
 	if (n == 0)
 		return (0);
 
 	va_start(ptr, n);
-	 
+
 	for (i = 0; i < n; i++)
 		Sum += va_arg(ptr, int);
 	va_end(ptr);
 
-	return Sum;
+	return (Sum);
 }
