be8ef4e400fa4ec55edd9e6f4e0017c5c0c706d1
Qureyshi <kamranqureyshi1998@gmail.com>
2024-03-22 14:05:57 +0000

    variadicfunc

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,4 +8,14 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 int _putchar(char c);
+/**
+ * struct format_types - Struct format_types
+ * @identifier: The conversion specifier
+ * @f: The function pointer
+ */
+typedef struct format_types
+{
+	char *identifier;
+	void (*f)(char *separator, va_list args);
+} f_dt;
 #endif
