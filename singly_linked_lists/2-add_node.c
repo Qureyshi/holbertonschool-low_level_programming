24d4a361a8817e5eba929ec363894eaf6aaa1430
Qureyshi <kamranqureyshi1998@gmail.com>
2024-04-01 13:06:52 +0000

    Lists2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
+ */
+
+list_t *add_node(list_t **head, const char *str){
+	char *dup;
+	int len;
+
+	dup = strdup(str);
+	len = strlen(dup);
+	list_t *lk = malloc(sizeof(list_t));
+	lk->str = dup;
+	lk->len = len;
+	lk->next = *head;
+	*head =  lk;
+	return(lk);
+}
