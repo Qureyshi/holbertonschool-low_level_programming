cf615d9166eb2948f61c41de6ffaedea7cc74222
Qureyshi <kamranqureyshi@gmail.com>
2024-04-04 12:50:08 +0000

    dlist

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,6 +13,8 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tmp = *h, *new;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 	
 	for (; idx != 1; idx--)
 	{
