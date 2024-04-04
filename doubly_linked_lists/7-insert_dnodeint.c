6c7605231b9ed85ae065bba9c58a072583506fbd
Qureyshi <kamranqureyshi@gmail.com>
2024-04-04 12:40:10 +0000

    dlist

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,23 @@
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp = *h, *new;
+
+	for (; idx != 1; idx--)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
+}
